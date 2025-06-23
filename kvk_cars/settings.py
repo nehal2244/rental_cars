3a082dffea8de169003258d55b39d55e12bdf1ae
Rimanshu <ambaderimanshu117@gmail.com>
2025-06-23 17:44:00 +0530

    Updated dynamic pricing logic, deployment settings, and static file paths

--- kvk_cars/settings.py
+++ kvk_cars/settings.py
@@ -23,8 +23,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 # DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() in ('true', '1')
 
 # ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '69.62.80.9,kvkcars.com,www.kvkcars.com').split(',')
-# DEBUG = False
-# ALLOWED_HOSTS = ['69.62.80.9', 'kvkcars.com', 'www.kvkcars.com']
+DEBUG = False
+ALLOWED_HOSTS = ['69.62.80.9', 'kvkcars.com', 'www.kvkcars.com']
 
 # DEBUG = True
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
